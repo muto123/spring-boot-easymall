b118a1ffc848160550243af35b545fd48b445001
lkj <3114484194@qq.com>
2023-01-12 21:30:20 +0800

    fifth commit

--- src/test/java/gitTest.java
+++ src/test/java/gitTest.java
@@ -5,7 +5,7 @@ public class gitTest {
     @Test
     public void test() {
         System.out.println("测试gitIdea");
-        System.out.println("测试不冲突合并,hot-fix到master");
+        System.out.println("测试不冲突合并");
     }
 
 }
