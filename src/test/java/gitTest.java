bee3a2045c33290f78659fa2669c78c1741426df
lkj <3114484194@qq.com>
2023-01-12 22:51:22 +0800

    master commit2

--- src/test/java/gitTest.java
+++ src/test/java/gitTest.java
@@ -8,6 +8,7 @@ public class gitTest {
         System.out.println("测试不冲突合并");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("github push test");
     }
 
 }
